6eff381039098c54f93c950fae001e5a7e7b270f
Luke Anderson <landerson61@gmail.com>
2020-10-07 08:17:22 +0100

    Add initial layout & design

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navigation = () => {
+  return (
+    <nav className='pa3 f3 link black underline pointer'>
+      <p style={{display: 'flex', justifyContent: 'flex-end'}}> Sign Out </p>
+    </nav>
+  )
+}
+
+export default Navigation;
